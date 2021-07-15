b4c6d2fe9b2f64b614d2bc4b115ddb236765669b
demassimo <87481244+demassimo@users.noreply.github.com>
2021-07-15 14:48:06 +0200

    Update installer_updater.bat

--- installer_updater.bat
+++ installer_updater.bat
@@ -1,6 +1,6 @@
 echo off
 mkdir gew_hub
 cd gew_hub 
-curl -o installer_update.bat https://pastebin.com/raw/zREBJ8Ws
+curl -o installer_update.bat https://raw.githubusercontent.com/demassimo/gew_hub_installer_updater/main/installer_update.bat
 pause
 start installer_update.bat
