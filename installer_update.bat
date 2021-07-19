6333dd7ac07831b0ecd5d1c34311cf30934c7bb6
demassimo <87481244+demassimo@users.noreply.github.com>
2021-07-19 09:03:58 +0200

    Update installer_update.bat

--- installer_update.bat
+++ installer_update.bat
@@ -4,8 +4,8 @@ echo created lazy file
 curl -o hub.pyw https://pastebin.com/raw/KpdkR2YB
 curl -o scripts.pyw https://pastebin.com/raw/GMqcwTpx
 curl -o config.pyw https://pastebin.com/raw/vcjkgUSi
-curl -o close.png https://cdn.discordapp.com/attachments/749252806010994689/865165707678973962/close.png  
-curl -o minimize.png https://cdn.discordapp.com/attachments/749252806010994689/865181655672422400/minimize.png
+curl -o close.png https://cdn.discordapp.com/attachments/749252806010994689/866575060403224596/close.png 
+curl -o minimize.png https://cdn.discordapp.com/attachments/749252806010994689/866575072097861682/minimize.png
 
 echo Installing pip files
 pip install pynput
